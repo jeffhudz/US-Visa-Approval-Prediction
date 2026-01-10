30c49c0dd25f1b85df0a6d9f6b3881dfa903446e
jeffhudz <godfrenam@yahoo.com>
2026-01-10 17:09:32 +0300

    Completed the config entity

--- README.md
+++ README.md
@@ -7,6 +7,19 @@ git add .
 git commit -m 'updated'
 git push origin main
 ```
+``` bash
 conda create -n visa python=3.8 -y
+```
+``` bash
 conda activate visa
+```
+``` bash
 pip install -r requirements.txt
+```
+
+## Workflows
+1.Constant
+2.Config
+3.Entity
+4.Component
+5.Pipeline
---
+++ us_visa/entity/config_entity.py
@@ -0,0 +1,24 @@
+import os
+from dataclasses import dataclass
+from datetime import datetime
+from us_visa.constant import *
+
+TIMESTAMP: str = datetime.now().strftime('%m%d%Y__%H%M%S')
+
+
+@dataclass
+class TrainingPipelineConfig:
+    pipeline_name: str = TRAINING_PIPELINE_NAME
+    artifact_dir: str = os.path.join(ARTIFACT_DIR,TIMESTAMP)
+    timestamp: str = TIMESTAMP
+
+
+trainingpipelineconfig: TrainingPipelineConfig = TrainingPipelineConfig()
+
+@dataclass
+class DataIngestionConfig:
+    data_ingestion_dir: str = os.path.join(trainingpipelineconfig.artifact_dir, DATA_INGESTION_COLLECTION_DIR_NAME) 
+    feature_store_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_FEATURE_STORE_DIR, FILENAME)
+    training_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TRAIN_FILE_NAME)
+    testing_file_path: str = os.path.join(data_ingestion_dir, DATA_INGESTION_INGESTED_DIR, TEST_FILE_NAME)
+    train_test_split_ratio: float = DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
