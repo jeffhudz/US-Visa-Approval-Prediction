0ac764192bcc63233392184fb6d3da9b98418b2e
jeffhudz <godfrenam@yahoo.com>
2026-01-19 22:35:09 +0300

    updated model yaml

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -41,3 +41,12 @@ DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_OBJECT_DIR: str = "transformed_object"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed_data"
 
+"""Model Trainer related constant start with MODEL_TRAINER_VAR_NAME"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"      
+MODEL_TRAINER_MODEL_FILE_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config","model_config.yaml")
+
+"""Model Evaluation related constant start with MODEL_EVALUATION_VAR_NAME"""
